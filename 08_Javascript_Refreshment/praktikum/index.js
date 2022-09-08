f698513c4a2796a8b59cee25401bc25e6b0e4294
Diopranata01 <bagusdio01@gmail.com>
2022-09-08 12:09:49 +0800

    Commit new changes in praktikum, index.js

--- 08_Javascript_Refreshment/praktikum/index.js
+++ 08_Javascript_Refreshment/praktikum/index.js
@@ -1,52 +1,79 @@
-// Soal
+// Soal 1
 var a = 5 ;
 let b = "kampus merdeka";
+let c = ""
 const nama = "Budi";
-let terdaftar = "false";
+let terdaftar = true;
 let lengkap_arr = [a, b, nama, terdaftar];
 
+
 function perkenalan (){
-    /* Let asal bersifat function scooping, untuk line 31 harus di panggil functionya */
+    /* Let asal bersifat function scooping, line 31 panggil function*/
     let asal = "indonesia"
     return console.log(
-        "perkenalkan nama saya" +
+        "perkenalkan nama saya " +
         nama +
-        "nomor urut" +
+        ", nomor urut " +
         a +
-        "sekarang sedang mengikuti "+
+        ", sekarang sedang mengikuti "+
         b + 
-        "berasal dari " +
+        ", berasal dari " +
         asal
     )
 }
 
+/* Agar ke print, ubah baris 5 boolean tanpa "" */
 if(terdaftar === true){
     console.log(nama + " terdaftar sebagai kegiatan kampus merdeka");
 }
 
-
-a = b
+/* a = b */
 /* nama = b */
 
-console.log("asal diakses = " + asal)
+console.log(`array index  ke 2 = ${lengkap_arr[1]}`)
+/* bisa juga  */
+const select = lengkap_arr[1]
+console.log("array index ke 2 = "+ select)
+
+console.log("array = " + lengkap_arr[2])
 console.log("a adalah = " + a)
 console.log("b adalah = " + b)
+perkenalan()
+
+// Soal 2
 
 
-// Jawaban
 
-/* 1. a. 
-let lengkap_arr = [10,2,3]
 
-const select = lengkap_arr[1]
-console.log(select) */
+// Soal 3
+const foo = ['Budi', 'Sita', 'Ayu' ];
 
-/* 1. b. 
-const nama = "Budi";
-let terdaftar = "true";
+[a,b,c] = foo;
 
-if(terdaftar = true){
-    console.log(nama + " terdaftar sebagai kegiatan kampus merdeka");
-}
+console.log(a,b,c);
 
- */
+// Soal 4
+
+let bdays = ['10-17', '05-19', '20-19']
+
+let new_bdays= bdays.map (bday => {
+    const [first, last] = bday.split('-');
+
+    return `${first}/${last}`;
+});
+
+console.log(new_bdays)
+
+// Soal 5
+let kali = [1,2,3,4,5,6]
+
+let hasil = kali.map(Element => Element * 2)
+
+console.log(hasil)
+
+// Soal 6
+let arr = [1.5, 2.56, 5.1, 12.33]
+
+let new_arr = arr.map(Element => Math.ceil(Element))
+
+console.log(new_arr)
