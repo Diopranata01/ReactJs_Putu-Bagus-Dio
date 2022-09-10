78ff0b1f94da09220fe99c4006cc21690d043438
Diopranata01 <bagusdio01@gmail.com>
2022-09-10 21:41:41 +0800

    Commit new changes in cleanCode.js

--- 09_Clean_Code/praktikum/README.md
+++ 09_Clean_Code/praktikum/README.md
@@ -1,5 +1,16 @@
-<<<<<<< HEAD
-#praktikum_Putu-Bagus-Dio
-=======
-#praktikum_Putu-Bagus-Dio
->>>>>>> 474cb2b56d69e9ddb845cbe873bdfbfe8c2aaad5
+# Praktikum_Putu-Bagus-Dio
+
+## Soal
+
+### 1. Problem Analysis
+### a. Berapa banyak kekurangan dalam penulisan code tersebut?
+### b. Bagian mana saja terjadi kekurangan tersebut?
+### c. Tuliskan alasan dari tiap kekurangan tersebut!
+
+### 2. Kode berikut ini dituliskan tanpa mengikuti kebiasaan - kebiasaan penulisan yang disarankan, ubahlah penulisan kode berikut menjadi lebih terbaca dan rapi!
+
+## Jawab
+
+### 1. Pada code nomor 1 ini, di awal pendefinisian class menggunakan variabel biasa, dimana penggunaan class biasanya di ikuti dengan method atau constructor, kemudian menulis isi dari object class tesebut, seperti id, username, password, menggunakan this.id, this.username, this.password. Selanjutnya pada userservice, saya tulis ulang untuk kekuranganya, dimana nanti users service akan menampilkan tambahan user branch.id dan melakukan return isi dari introduction pada userservice.
+
+### 2. 
---
+++ 09_Clean_Code/praktikum/cleanCode.js
@@ -0,0 +1,83 @@
+// Soal 1
+/* class user{
+    var id
+    var username
+    var password
+}
+
+class userservice{
+    user[] user = [];
+
+    user[] getallusers(){
+        return users;
+    }
+
+    user getuserbyid(userid){
+        return users.filter(userid)
+    }
+} */
+
+// Jawaban 1
+
+class user{
+    constructor (id, name, password){
+        this.id = id
+        this.name = name
+        this.password = password
+    }
+}
+
+class userservice extends user{
+    constructor (id , name, password, branchId){
+        super (id, name, password)
+        this.branchId = branchId
+    }
+    introduction(){
+        return `My id is ${this.id}, ${this.name}, and my Branch Id is ${this.branchId}`
+    }
+
+}
+
+let newUser = new user(01,"newAdmin", 1234)
+const newUser_Service = new userservice (02,'newAdmin1', 1234, 33)
+
+console.log(newUser.id + " " + newUser.name)
+console.log(newUser_Service.id + " " + newUser_Service.name + " " + newUser_Service.branchId)
+console.log(newUser_Service.introduction())
+
+
+//Soal 2
+
+class kendaraan {
+    constructor(totalroda, kecepatanperjam){
+        this.totalroda = 0
+        this.kecepatanperjam = 0
+    }
+}
+
+class mobil extends kendaraan{
+
+    berjalan(){
+        this.tambahKecepatan = 1
+    }
+
+    tambahkecepatan(berjalan)
+
+    /* berjalan(){
+        return tambahkecepatan(10)
+    }
+
+    tambahkecepatan(kecepatanbaru){
+        kecepatanperjam = kecepatanperjam + kecepatanbaru
+    } */
+}
+
+console.log(berjalan)
+
+   /*  mobilcepat = new mobil()
+    mobilcepat.berjalan()
+    mobilcepat.berjalan()
+    mobilcepat.berjalan()
+
+    mobillamban = new mobil()
+    mobillamban.berjalan() */
