5c8f0c7e615ef8d987c5a9b9c3dd5ab5ace33286
Diopranata01 <bagusdio01@gmail.com>
2022-09-23 01:48:09 +0800

    Commmit new changes in ./13_Event_Handling and new Files in ./14_React_Hook

---
+++ 14_React_Hook/praktikum/to-do_App/src/App.js
@@ -0,0 +1,16 @@
+import IsiTodo from './components/IsiTodo';
+
+function App() {
+  return (
+    <div className="App">
+      <><br/><br/><br/><br/>
+        <header>
+          <h1>todos</h1>
+        </header>
+        <IsiTodo/>
+      </>
+    </div>
+  );
+}
+
+export default App;
